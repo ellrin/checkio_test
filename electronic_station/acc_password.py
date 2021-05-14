ae352585d4edae6d0ced1b64e0dc8199e4f3d8c9
ellrin <j1542727@gmail.com>
2021-05-14 13:43:03 +0800

    upload acc password

---
+++ electronic_station/acc_password.py
@@ -0,0 +1,19 @@
+import re
+def is_acceptable_password(password: str) -> bool:
+    if len(password) <= 6:
+        return False
+    else:
+        return False if re.search('[0-9]', password) == None else True
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
